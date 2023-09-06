242620a33857c928e0f6c96b3ace64197d79a1c8
xwhuj <xwhujob@163.com>
2023-09-06 16:48:43 +0800

    1

--- app/src/main/java/com/loan/golden/cash/money/loan/ui/fragment/mine/MineFragment.kt
+++ app/src/main/java/com/loan/golden/cash/money/loan/ui/fragment/mine/MineFragment.kt
@@ -1,7 +1,11 @@
 package com.loan.golden.cash.money.loan.ui.fragment.mine
 
 import android.os.Bundle
+import com.loan.golden.cash.money.loan.R
 import com.loan.golden.cash.money.loan.app.base.BaseFragment
+import com.loan.golden.cash.money.loan.app.util.nav
+import com.loan.golden.cash.money.loan.app.util.navigateAction
+import com.loan.golden.cash.money.loan.app.util.setOnclickNoRepeat
 import com.loan.golden.cash.money.loan.databinding.FragmentMineBinding
 import com.loan.golden.cash.money.loan.ui.viewmodel.MineViewModel
 
@@ -11,7 +15,26 @@ import com.loan.golden.cash.money.loan.ui.viewmodel.MineViewModel
  * @Describe    : 我的
  */
 class MineFragment : BaseFragment<MineViewModel, FragmentMineBinding>() {
+
+
     override fun initView(savedInstanceState: Bundle?) {
 
+    }
+
+    override fun onBindViewClick() {
+        super.onBindViewClick()
+        setOnclickNoRepeat(
+            mBind.llMineMyOrder,
+            mBind.llMinePrivacyPolicy,
+            mBind.llMineTermsCondition,
+            mBind.llMineLoanProduct,
+            mBind.llMineServiceEmail
+        ) {
+            when (it) {
+                mBind.llMinePrivacyPolicy -> {
+                    nav().navigateAction(R.id.action_login_fragment_privacy_policy)
+                }
+            }
+        }
     }
 }
