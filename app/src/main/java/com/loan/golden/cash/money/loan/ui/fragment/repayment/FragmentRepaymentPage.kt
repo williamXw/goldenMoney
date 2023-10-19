27249c7a8d55984461d94f8a4fd88f80c2d6a6a8
xwhuj <xwhujob@163.com>
2023-10-19 13:20:38 +0800

    1

--- app/src/main/java/com/loan/golden/cash/money/loan/ui/fragment/repayment/FragmentRepaymentPage.kt
+++ app/src/main/java/com/loan/golden/cash/money/loan/ui/fragment/repayment/FragmentRepaymentPage.kt
@@ -7,9 +7,13 @@ import android.webkit.JsResult
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
 import com.loan.golden.cash.money.loan.app.base.BaseFragment
+import com.loan.golden.cash.money.loan.app.util.nav
 import com.loan.golden.cash.money.loan.databinding.FragmentRepaymentPageBinding
 import com.loan.golden.cash.money.loan.ui.viewmodel.RepaymentViewModel
+import me.hgj.mvvmhelper.util.LogUtils
 
 /**
  * @Author      : hxw
@@ -19,14 +23,21 @@ import com.loan.golden.cash.money.loan.ui.viewmodel.RepaymentViewModel
 class FragmentRepaymentPage : BaseFragment<RepaymentViewModel, FragmentRepaymentPageBinding>() {
 
     override fun initView(savedInstanceState: Bundle?) {
-        val url = arguments?.getString("url")
+        url = arguments?.getString("url")
         initWebView(url)
+
+        /** 监听系统返回键 */
+        activity?.onBackPressedDispatcher?.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                nav().navigateUp()
+            }
+        })
     }
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun initWebView(url: String?) {
-        url?.let { mBind.webview.loadUrl(it) }
-        val webSettings = mBind.webview.settings
+        url?.let { mBind.webViewRepayment.loadUrl(it) }
+        val webSettings = mBind.webViewRepayment.settings
         webSettings.userAgentString = webSettings.userAgentString + "Android-Pigeon"
         webSettings.javaScriptEnabled = true//允许使用js
         webSettings.cacheMode = WebSettings.LOAD_NO_CACHE//不使用缓存，只从网络获取数据.
@@ -37,9 +48,21 @@ class FragmentRepaymentPage : BaseFragment<RepaymentViewModel, FragmentRepayment
         webSettings.loadWithOverviewMode = false//和setUseWideViewPort(true)一起解决网页自适应问题
         webSettings.domStorageEnabled = true//DOM Storage
         webSettings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW//加载https和http混合模式
-        mBind.webview.webChromeClient = webChromeClient
+        mBind.webViewRepayment.webChromeClient = webChromeClient
+        mBind.webViewRepayment.webViewClient = webViewClient
     }
 
+    private val webViewClient: WebViewClient = object : WebViewClient() {
+        override fun onPageFinished(view: WebView, url: String) {//页面加载完成
+            LogUtils.debugInfo("页面加载完成")
+        }
+
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            return super.shouldOverrideUrlLoading(view, url)
+        }
+    }
+
+    private var url: String? = ""
     private val webChromeClient: WebChromeClient = object : WebChromeClient() {
 
         override fun onJsAlert(view: WebView?, url: String?, message: String?, result: JsResult?): Boolean {
@@ -52,7 +75,9 @@ class FragmentRepaymentPage : BaseFragment<RepaymentViewModel, FragmentRepayment
 
         override fun onProgressChanged(view: WebView?, newProgress: Int) {
             super.onProgressChanged(view, newProgress)
-            mBind.progressBar.visibility = View.VISIBLE
+            if (mBind.progressBar != null) {
+                mBind.progressBar.visibility = View.VISIBLE
+            }
             mBind.progressBar.progress = newProgress
             if (newProgress == 100) {
                 mBind.progressBar.visibility = View.GONE
@@ -60,10 +85,15 @@ class FragmentRepaymentPage : BaseFragment<RepaymentViewModel, FragmentRepayment
         }
     }
 
-    override fun onDestroyView() {
+    override fun onStart() {
+        mBind.webViewRepayment.reload()
+        super.onStart()
+    }
+
+    override fun onDestroyView()  {
+        mBind.webViewRepayment.loadDataWithBaseURL(null, "", "text/html", "utf-8", null)
+        mBind.webViewRepayment.clearHistory()
+        mBind.webViewRepayment.destroy()
         super.onDestroyView()
-        mBind.webview.loadDataWithBaseURL(null, "", "text/html", "utf-8", null)
-        mBind.webview.clearHistory()
-        mBind.webview.destroy()
     }
 }
